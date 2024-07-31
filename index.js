08720e2cbf565c9857536a8d118d4b3950f9b100
Rony Rizk <rony_rizk@live.com>
2024-07-31 17:22:16 +0300

    Make a Selct dynamic with data retrieved from API

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ function getStudents()
 {
     fetch('https://trainingapi-5y1t.onrender.com/students')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(data => {
+        var select_users = document.getElementById('select_users');
+        data.forEach(st => {
+            var opt = document.createElement('option');
+            opt.innerHTML = st.name;
+            select_users.appendChild(opt);
+        });
+      })
 }
 
 getStudents();
